116f3991c2a2ed6215b85d6639ab5c7d5d35aac4
Rodrigo Neumann <rodrigoneumann@gmail.com>
2020-03-30 12:08:07 +0100

    postgres database added to heroku

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "asdfg1234")
+
+os.environ.setdefault("DATABASE_URL", "postgres://sgsirtopvhzhem:5d184e004db12b3cf9518e74c967b27608dccc0c0cb8528c0e5d2571c7e95029@ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/d54cgncc58tb3j")
